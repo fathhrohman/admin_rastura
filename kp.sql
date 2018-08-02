cb4260c2e7b4ab2945d29d9375cbf92f0e125527
fathhrohman <fathur1097@gmail.com>
2018-08-02 21:52:48 +0700

    sql baru

--- kp.sql
+++ kp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 31, 2018 at 02:19 PM
+-- Generation Time: Aug 02, 2018 at 04:52 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -57,8 +57,33 @@ CREATE TABLE `berita` (
 --
 
 INSERT INTO `berita` (`id`, `judul`, `link`) VALUES
-(0, 'asdfasd', 'asdfas'),
+(12, 'losdaf', 'www.www.www'),
 (123, 'judul', 'judul');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `portfolio`
+--
+
+CREATE TABLE `portfolio` (
+  `id` int(2) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+INSERT INTO `portfolio` (`id`, `gambar`, `judul`, `deskripsi`) VALUES
+(1, '1478.jpg', 'gkll', 'fasd'),
+(2, '4.PNG', 'jdl', 'dse'),
+(3, '', 'j3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus risus at sapien rhoncus, nec aliquet nibh lobortis. Pellentesque pellentesque arcu nec leo cursus egestas id sit amet ante.'),
+(4, '', '4j', '4d'),
+(5, '', '5j', '5d'),
+(6, '', '6j', '6d');
 
 --
 -- Indexes for dumped tables
@@ -68,6 +93,12 @@ INSERT INTO `berita` (`id`, `judul`, `link`) VALUES
 -- Indexes for table `berita`
 --
 ALTER TABLE `berita`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `portfolio`
+--
+ALTER TABLE `portfolio`
   ADD PRIMARY KEY (`id`);
 COMMIT;
 
